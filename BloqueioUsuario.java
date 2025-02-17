225673a103f317880b7b228258b2bc4fec03111a
Cleidi Rocha da Silva - Unicred do Brasil <cleidi.silva@e-unicred.com.br>
2025-02-17 08:47:41 -0300

    desafio 11 - bloqueio usuário

---
+++ BloqueioUsuario.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class BloqueioUsuario {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String usuarioCorreto = "aluno";
+        String senhaCorreta = "segredo";
+        int tentativas = 0;
+        final int MAX_TENTATIVAS = 3;
+
+        while (tentativas < MAX_TENTATIVAS) {
+            System.out.print("\nDigite o usuário: ");
+            String usuario = scanner.nextLine();
+
+            System.out.print("Digite a senha: ");
+            String senha = scanner.nextLine();
+
+            if (usuario.equals(usuarioCorreto) && senha.equals(senhaCorreta)) {
+                System.out.println("\nBem-vindo!");
+                break; // Sai do loop se o login for bem-sucedido
+            } else {
+                tentativas++;
+                if (tentativas < MAX_TENTATIVAS) {
+                    System.out.println("Usuário ou senha incorretos. Tente novamente.");
+                } else {
+                    System.out.println("\nConta bloqueada após " + MAX_TENTATIVAS + " tentativas.");
+                }
+            }
+        }
+
+        scanner.close();
+    }
+}
