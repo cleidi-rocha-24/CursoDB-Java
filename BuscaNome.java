bca6019d3c12a069617af9a6ab578070ef9daaa5
Cleidi Rocha da Silva - Unicred do Brasil <cleidi.silva@e-unicred.com.br>
2025-02-17 08:52:09 -0300

    Desafio 13 - busca nome

---
+++ BuscaNome.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class BuscaNome {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] nomes = new String[10]; 
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.print("Informe o nome " + (i + 1) + ": ");
+            nomes[i] = scanner.nextLine();
+        }
+
+        // Leitura do nome a ser buscado
+        System.out.print("Informe um nome para buscar no vetor: ");
+        String nomeBusca = scanner.nextLine();
+
+        boolean achei = false;
+        for (int i = 0; i < nomes.length; i++) {
+            if (nomes[i].equalsIgnoreCase(nomeBusca)) {
+                achei = true;
+                break; 
+            }
+        }
+
+        // Exibição do resultado
+        if (achei) {
+            System.out.println("Achei");
+        } else {
+            System.out.println("Não achei");
+        }
+
+        scanner.close();
+    }
+}
